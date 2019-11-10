3a11086d0121716ec13cd6499a6dfc55c39289cf
Tu, Fred <frederic.tu@gmail.com>
2019-11-10 23:34:56 +0800

    SAMPLE : @ExecutionSpan adoption (need refactoring to avoid re-entrance issue)

---
+++ sample-microservices/service-a/src/main/java/com/github/frtu/logs/example/demo/PrinterUtil.java
@@ -0,0 +1,24 @@
+package com.github.frtu.logs.example.demo;
+
+import com.github.frtu.logs.tracing.aspect.ExecutionSpan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PrinterUtil {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PrinterUtil.class);
+
+    @ExecutionSpan
+    public String formatString(String helloTo) {
+        String helloStr = String.format("Hello, %s!", helloTo);
+        printHello(helloStr);
+
+        return helloStr;
+    }
+
+    @ExecutionSpan
+    public void printHello(String helloStr) {
+        LOGGER.info(helloStr);
+    }
+}
