1e05bbd5495d21a793395852ec079d0d3e60780e
Tu, Fred <frederic.tu@gmail.com>
2019-11-16 09:09:28 +0800

    Allow to have a short (by default) or a long name by configuration trace.full.classname:#{environment.TRACE_FULL_CLASSNAME}

--- logger-libraries/logger-core/src/main/java/com/github/frtu/logs/tracing/annotation/ExecutionSpanAspect.java
+++ logger-libraries/logger-core/src/main/java/com/github/frtu/logs/tracing/annotation/ExecutionSpanAspect.java
@@ -4,21 +4,30 @@ import com.github.frtu.logs.tracing.core.TraceUtil;
 import io.opentracing.Scope;
 import io.opentracing.Tracer;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+/**
+ * Aspect that create a {@link io.opentracing.Span} around the annotated method using {@link ExecutionSpan}.
+ *
+ * @return
+ * @since 0.9.0
+ */
 @Aspect
 @Component
 public class ExecutionSpanAspect {
     private static final Logger LOGGER = LoggerFactory.getLogger(ExecutionSpanAspect.class);
 
     public static final String MDC_KEY_TRACE_ID = "trace.id";
+
+    @Value("${trace.full.classname:#{environment.TRACE_FULL_CLASSNAME ?: false}}")
+    boolean isFullClassName;
 
     @Autowired
     private Tracer tracer;
@@ -28,18 +37,25 @@ public class ExecutionSpanAspect {
 
     @Around("@annotation(ExecutionSpan)")
     public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
-        final Signature signature = joinPoint.getSignature();
-
-        String signatureName = new StringBuilder()
-                .append(signature.getDeclaringTypeName())
-                .append('.')
-                .append(signature.getName()).toString();
+        final Class declaringType = joinPoint.getSignature().getDeclaringType();
+        String signatureName = getName(declaringType, joinPoint.getSignature().getName());
 
         try (Scope scope = tracer.buildSpan(signatureName).startActive(true)) {
             final String traceId = traceUtil.getTraceId(scope.span());
             MDC.put(MDC_KEY_TRACE_ID, traceId);
+
             LOGGER.debug("Creating span around signature={} and traceId={}", signatureName, traceId);
             return joinPoint.proceed();
         }
+    }
+
+    String getName(Class declaringType, String methodName) {
+        final StringBuilder stringBuilder = new StringBuilder();
+        if (isFullClassName) {
+            stringBuilder.append(declaringType.getCanonicalName());
+        } else {
+            stringBuilder.append(declaringType.getSimpleName());
+        }
+        return stringBuilder.append('.').append(methodName).toString();
     }
 }
