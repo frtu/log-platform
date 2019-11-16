06afb6bc8ae887445f687f5ec6891781bafa2e21
Tu, Fred <frederic.tu@gmail.com>
2019-11-16 09:10:19 +0800

    [JAVADOC]

--- logger-libraries/logger-core/src/main/java/com/github/frtu/logs/tracing/annotation/ExecutionSpan.java
+++ logger-libraries/logger-core/src/main/java/com/github/frtu/logs/tracing/annotation/ExecutionSpan.java
@@ -5,6 +5,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Add a new {@link io.opentracing.Span} into current {@link io.opentracing.Tracer}.
+ *
+ * @return
+ * @since 0.9.0
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD})
 public @interface ExecutionSpan {
--- logger-libraries/logger-core/src/main/java/com/github/frtu/logs/tracing/core/TraceUtil.java
+++ logger-libraries/logger-core/src/main/java/com/github/frtu/logs/tracing/core/TraceUtil.java
@@ -2,6 +2,18 @@ package com.github.frtu.logs.tracing.core;
 
 import io.opentracing.Span;
 
+/**
+ * Utility class for Trace
+ *
+ * @author fred
+ * @since 0.9.0
+ */
 public interface TraceUtil {
+    /**
+     * Extract a Trace ID from a {@link Span}
+     *
+     * @param span the current span
+     * @return Trace ID
+     */
     String getTraceId(Span span);
 }
