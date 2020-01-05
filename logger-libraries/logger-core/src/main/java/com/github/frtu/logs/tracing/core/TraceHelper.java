5f85b71d26c31b405aa74f38d8391335f42f7d03
Tu, Fred <frederic.tu@gmail.com>
2020-01-05 14:45:12 +0800

    [Small refactor] Add comment and refer to opentracing convention

--- logger-libraries/logger-core/src/main/java/com/github/frtu/logs/tracing/core/TraceHelper.java
+++ logger-libraries/logger-core/src/main/java/com/github/frtu/logs/tracing/core/TraceHelper.java
@@ -19,7 +19,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class TraceHelper {
     private static final Logger LOGGER = LoggerFactory.getLogger(TraceHelper.class);
-    public static final String LOG_KEY_ERROR_MSG = "error";
+
+    /**
+     * @see <a href="https://github.com/opentracing/specification/blob/master/semantic_conventions.md#span-tags-table">OpenTracing Tag Semantic</a>
+     */
+    public static final String TAG_KEY_ERROR_FLAG = "error";
+
+    /**
+     * @see <a href="https://github.com/opentracing/specification/blob/master/semantic_conventions.md#captured-errors">OpenTracing Error Semantic</a>
+     */
+    public static final String LOG_KEY_EVENT = "event";
+    public static final String LOG_KEY_ERROR_MSG = "message";
 
     @Autowired
     Tracer tracer;
@@ -29,16 +39,29 @@ public class TraceHelper {
         tracer.activeSpan().log(ImmutableMap.of(key, value));
     }
 
+    /**
+     * Flag this span as error. OPTIONALLY add an error msg.
+     *
+     * @param errorMsg can be null
+     * @since 0.9.4
+     */
     public void flagError(String errorMsg) {
         flagError(tracer.activeSpan(), errorMsg);
     }
 
+    /**
+     * Flag this span as error. OPTIONALLY add an error msg.
+     *
+     * @param activeSpan the active span
+     * @param errorMsg   can be null
+     * @since 0.9.4
+     */
     public void flagError(final Span activeSpan, final String errorMsg) {
         LOGGER.error("Adding ERROR to current span. Error message: {}", errorMsg, new Exception(errorMsg));
         if (activeSpan != null) {
-            activeSpan.setTag("error", true);
+            activeSpan.setTag(TAG_KEY_ERROR_FLAG, true);
             if (errorMsg != null) {
-                activeSpan.log(ImmutableMap.of(LOG_KEY_ERROR_MSG, errorMsg));
+                activeSpan.log(ImmutableMap.of(LOG_KEY_EVENT, TAG_KEY_ERROR_FLAG, LOG_KEY_ERROR_MSG, errorMsg));
             }
         } else {
             LOGGER.error("Span MUST NOT be null ! Loosing span info !!");
