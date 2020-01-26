93551641433a7314be979195dd4221e15e7ee219
Tu, Fred <frederic.tu@gmail.com>
2020-01-26 23:24:08 +0800

    Enable tracing for service-b

--- sample-microservices/service-b/src/main/java/com/github/frtu/logs/example/demo/DemoApplication.java
+++ sample-microservices/service-b/src/main/java/com/github/frtu/logs/example/demo/DemoApplication.java
@@ -3,10 +3,12 @@ package com.github.frtu.logs.example.demo;
 import com.github.frtu.logs.config.ConfigAll;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import({ConfigAll.class})
+@ComponentScan(basePackages = {"com.github.frtu.metrics", "com.github.frtu.logs.example"})
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
