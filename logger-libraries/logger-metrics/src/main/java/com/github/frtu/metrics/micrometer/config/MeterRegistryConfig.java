7c868b15706583c605f5946e027baf3ed7af2fae
Tu, Fred <frederic.tu@gmail.com>
2020-01-26 21:46:30 +0800

    Refactor micrometer package add /actuator/info to service-a

--- logger-libraries/logger-metrics/src/main/java/com/github/frtu/metrics/micrometer/config/MeterRegistryConfig.java
+++ logger-libraries/logger-metrics/src/main/java/com/github/frtu/metrics/micrometer/config/MeterRegistryConfig.java
@@ -1,23 +1,33 @@
-package com.github.frtu.metrics.prometheus;
+package com.github.frtu.metrics.micrometer.config;
 
+import com.github.frtu.logs.config.condition.AopConditionalOnClass;
 import com.github.frtu.logs.core.ApplicationMetadata;
 import io.micrometer.core.aop.TimedAspect;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * Micrometer registry configuration
  */
 @Configuration
-public class RegistryConfig {
+public class MeterRegistryConfig {
     @Bean
     MeterRegistryCustomizer<MeterRegistry> metricsCommonTags(ApplicationMetadata applicationMetadata) {
-        return registry -> registry.config().commonTags("app.name", applicationMetadata.getApplicationName());
+        final String applicationName = applicationMetadata.getApplicationName();
+        return registry -> registry.config().commonTags("app.name", applicationName);
     }
 
+    /**
+     * Creating AOP aspect watching for io.micrometer.core.annotation.Timed
+     *
+     * @param registry {@link MeterRegistry}
+     * @return TimedAspect
+     */
     @Bean
+    @Conditional(AopConditionalOnClass.class)
     TimedAspect timedAspect(MeterRegistry registry) {
         return new TimedAspect(registry);
     }
--- sample-microservices/service-a/src/main/java/com/github/frtu/logs/example/demo/DemoApplication.java
+++ sample-microservices/service-a/src/main/java/com/github/frtu/logs/example/demo/DemoApplication.java
@@ -4,10 +4,12 @@ import com.github.frtu.logs.config.ConfigFluentD;
 import com.github.frtu.logs.config.ConfigTracingOnly;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import({ConfigTracingOnly.class, ConfigFluentD.class})
+@ComponentScan(basePackages = {"com.github.frtu.metrics", "com.github.frtu.logs.example"})
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
