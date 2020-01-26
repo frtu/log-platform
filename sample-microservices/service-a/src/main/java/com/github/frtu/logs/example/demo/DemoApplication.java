eea2ab1cee5753c6fad822c2f95184e4d4ed1f84
Tu, Fred <frederic.tu@gmail.com>
2020-01-26 09:37:18 +0800

    Append FluentD configuration

--- sample-microservices/service-a/src/main/java/com/github/frtu/logs/example/demo/DemoApplication.java
+++ sample-microservices/service-a/src/main/java/com/github/frtu/logs/example/demo/DemoApplication.java
@@ -1,12 +1,13 @@
 package com.github.frtu.logs.example.demo;
 
+import com.github.frtu.logs.config.ConfigFluentD;
 import com.github.frtu.logs.config.ConfigTracingOnly;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses = {ConfigTracingOnly.class, DemoApplication.class})
+@Import({ConfigTracingOnly.class, ConfigFluentD.class})
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
