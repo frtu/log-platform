700b57eccffe533a9c2de2f20546f3d99f8f4361
Tu, Fred <frederic.tu@gmail.com>
2020-04-04 11:25:21 +0800

    - Support null and allow to null key or value into Map (attention Many null key will mutually override) - Rename getJson

--- logger-libraries/logger-core/src/main/java/com/github/frtu/logs/core/StructuredLogger.java
+++ logger-libraries/logger-core/src/main/java/com/github/frtu/logs/core/StructuredLogger.java
@@ -3,12 +3,11 @@ package com.github.frtu.logs.core;
 import ch.qos.logback.more.appenders.marker.MapMarker;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.AbstractMap;
-import java.util.Collections;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -19,27 +18,67 @@ import java.util.stream.Stream;
  * @author Frédéric TU
  * @since 1.0.2
  */
+@Slf4j
 public class StructuredLogger {
     public static final String BASE_FORMAT = "{}";
 
     private ObjectMapper mapper = new ObjectMapper();
     private Logger logger;
 
+    /**
+     * Create a KV {@link Map.Entry} using parameters.
+     *
+     * @param key   Key for this entry (support null)
+     * @param value Value for this entry (support null)
+     * @param <K>   Key type
+     * @param <V>   Value type
+     * @return null if one of key or value is null
+     */
     public static <K, V> Map.Entry<K, V> entry(K key, V value) {
-        final AbstractMap.SimpleImmutableEntry<K, V> entry = new AbstractMap.SimpleImmutableEntry<>(key, value);
+        LOGGER.trace("Add key:{} value:{{}", key, value);
+        AbstractMap.SimpleImmutableEntry<K, V> entry = new AbstractMap.SimpleImmutableEntry<>(key, value);
         return entry;
     }
 
-    public static Map unmodifiableMap(Map.Entry... entries) {
+    /**
+     * Create a {@link Map} from the
+     *
+     * @param entries
+     * @return return null if entries is null
+     */
+    public static <K, V> Map<K, V> map(Map.Entry<K, V>... entries) {
+        if (entries == null) {
+            LOGGER.trace("entries is null");
+            return null;
+        }
+        LOGGER.trace("Adding entries size:{}", entries.length);
+
+        Set<K> nullValues = new HashSet<>();
+        final Map<K, V> result = Stream.of(entries)
+                .filter(entry -> entry != null)
+                .filter(kvEntry -> {
+                    // Attention map.merge() cannot support null value
+                    if (kvEntry.getValue() == null) {
+                        nullValues.add(kvEntry.getKey());
+                        return false;
+                    }
+                    return true;
+                })
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        // Add null value for key back here
+        nullValues.forEach(key -> result.put(key, null));
+        return result;
+    }
+
+    public static <K, V> Map<K, V> unmodifiableMap(Map.Entry<K, V>... entries) {
+        if (entries == null) {
+            LOGGER.trace("entries is null");
+            return null;
+        }
         return Collections.unmodifiableMap(map(entries));
     }
 
-    public static Map map(Map.Entry... entries) {
-        return Stream.of(entries)
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-    }
-
-    public String getData(Map map) {
+    private String getJson(Map map) {
         String data;
         try {
             data = mapper.writeValueAsString(map);
@@ -68,7 +107,7 @@ public class StructuredLogger {
     public void trace(String format, Map.Entry... entries) {
         if (this.logger.isTraceEnabled()) {
             final Map map = unmodifiableMap(entries);
-            this.logger.trace(new MapMarker("", map), format, getData(map));
+            this.logger.trace(new MapMarker("", map), format, getJson(map));
         }
     }
 
@@ -79,7 +118,7 @@ public class StructuredLogger {
     public void debug(String format, Map.Entry... entries) {
         if (this.logger.isDebugEnabled()) {
             final Map map = unmodifiableMap(entries);
-            this.logger.debug(new MapMarker("", map), format, getData(map));
+            this.logger.debug(new MapMarker("", map), format, getJson(map));
         }
     }
 
@@ -90,7 +129,7 @@ public class StructuredLogger {
     public void info(String format, Map.Entry... entries) {
         if (this.logger.isInfoEnabled()) {
             final Map map = unmodifiableMap(entries);
-            this.logger.info(new MapMarker("", map), format, getData(map));
+            this.logger.info(new MapMarker("", map), format, getJson(map));
         }
     }
 
@@ -101,7 +140,7 @@ public class StructuredLogger {
     public void warn(String format, Map.Entry... entries) {
         if (this.logger.isWarnEnabled()) {
             final Map map = unmodifiableMap(entries);
-            this.logger.warn(new MapMarker("", map), format, getData(map));
+            this.logger.warn(new MapMarker("", map), format, getJson(map));
         }
     }
 
@@ -112,7 +151,7 @@ public class StructuredLogger {
     public void error(String format, Map.Entry... entries) {
         if (this.logger.isErrorEnabled()) {
             final Map map = unmodifiableMap(entries);
-            this.logger.error(new MapMarker("", map), format, getData(map));
+            this.logger.error(new MapMarker("", map), format, getJson(map));
         }
     }
 }
--- logger-libraries/logger-core/src/test/java/com/github/frtu/logs/core/StructuredLoggerTest.java
+++ logger-libraries/logger-core/src/test/java/com/github/frtu/logs/core/StructuredLoggerTest.java
@@ -1,15 +1,24 @@
 package com.github.frtu.logs.core;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
 import java.util.Map;
 
 import static org.junit.Assert.*;
 
+@Slf4j
 public class StructuredLoggerTest {
 
     @Test
-    public void entry() {
+    public void mapNull() {
+        final Map<String, String> map = StructuredLogger.unmodifiableMap(null);
+        LOGGER.debug("Result map:{}", map);
+        assertNull(map);
+    }
+
+    @Test
+    public void entryAndMap() {
         //--------------------------------------
         // 1. Prepare data
         //--------------------------------------
@@ -26,5 +35,64 @@ public class StructuredLoggerTest {
         //--------------------------------------
         assertEquals(key, entry.getKey());
         assertEquals(value, entry.getValue());
+
+        //--------------------------------------
+        // 4. Test map
+        //--------------------------------------
+        final Map<String, String> map = StructuredLogger.unmodifiableMap(entry);
+        LOGGER.debug("Result map:{}", map);
+        assertEquals(value, map.get(key));
+    }
+
+    @Test
+    public void entryAndMapNullValue() {
+        //--------------------------------------
+        // 1. Prepare data
+        //--------------------------------------
+        final String key = "key";
+
+        //--------------------------------------
+        // 2. Execute
+        //--------------------------------------
+        final Map.Entry<String, String> entry = StructuredLogger.entry(key, null);
+
+        //--------------------------------------
+        // 3. Validate
+        //--------------------------------------
+        assertEquals(key, entry.getKey());
+        assertNull(entry.getValue());
+
+        //--------------------------------------
+        // 4. Test map
+        //--------------------------------------
+        final Map<String, String> map = StructuredLogger.unmodifiableMap(entry);
+        LOGGER.debug("Result map:{}", map);
+        assertNull(map.get(key));
+    }
+
+    @Test
+    public void entryAndMapNullKey() {
+        //--------------------------------------
+        // 1. Prepare data
+        //--------------------------------------
+        final String value = "value";
+
+        //--------------------------------------
+        // 2. Execute
+        //--------------------------------------
+        final Map.Entry<String, String> entry = StructuredLogger.entry(null, value);
+
+        //--------------------------------------
+        // 3. Validate
+        //--------------------------------------
+        assertNull(entry.getKey());
+        assertEquals(value, entry.getValue());
+
+        //--------------------------------------
+        // 4. Test map
+        //--------------------------------------
+        final Map<String, String> map = StructuredLogger.unmodifiableMap(entry);
+        LOGGER.debug("Result map:{}", map);
+        assertEquals(value, map.get(null));
     }
 }
