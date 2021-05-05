3c6b8e16b51353dc7baf45bc28398cfc5fd2b49c
Tu, Fred <frederic.tu@gmail.com>
2021-05-05 22:25:19 +0800

    DOC - add javadoc for Composite Configuration

--- logger-libraries/logger-async/src/test/java/com/github/frtu/logs/Log4j2CompositeConfiguration.java
+++ logger-libraries/logger-async/src/test/java/com/github/frtu/logs/Log4j2CompositeConfiguration.java
@@ -3,6 +3,9 @@ package com.github.frtu.logs;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ * @see <a href="https://logging.apache.org/log4j/2.x/manual/configuration.html#CompositeConfiguration">Composite Configuration</a>
+ */
 public class Log4j2CompositeConfiguration {
     public static void main(String[] args) {
         System.setProperty("log4j.configurationFile", "log4j2-core.xml,log4j2-test.xml");
