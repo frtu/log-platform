8cb8a6e9dc0df06348b245021a443913d19d85aa
Tu, Fred <frederic.tu@gmail.com>
2021-08-15 23:11:29 +0800

    doc - fix javadoc&

--- logger-libraries/logger-core/src/main/java/com/github/frtu/logs/core/metadata/DefaultApplicationMetadataFactory.java
+++ logger-libraries/logger-core/src/main/java/com/github/frtu/logs/core/metadata/DefaultApplicationMetadataFactory.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * Default {@link ApplicationMetadata} factory. Configure application name for logs & metrics.
+ * Default {@link ApplicationMetadata} factory. Configure application name for logs and metrics.
  * <p>
  * Allow override by creating another bean using name {@link #BEAN_NAME}
  *
