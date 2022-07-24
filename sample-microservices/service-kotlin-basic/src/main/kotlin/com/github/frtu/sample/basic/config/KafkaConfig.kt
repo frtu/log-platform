31f1483abc411a8aa45d38602c84fff6bc3ba7fd
Tu, Fred <frederic.tu@gmail.com>
2022-07-25 00:18:33 +0800

    feat: adding KafkaTelemetry initialization

--- sample-microservices/service-kotlin-basic/src/main/kotlin/com/github/frtu/sample/basic/config/KafkaConfig.kt
+++ sample-microservices/service-kotlin-basic/src/main/kotlin/com/github/frtu/sample/basic/config/KafkaConfig.kt
@@ -1,5 +1,7 @@
 package com.github.frtu.sample.basic.config
 
+import io.opentelemetry.api.OpenTelemetry
+import io.opentelemetry.instrumentation.kafkaclients.KafkaTelemetry
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -9,14 +11,27 @@ import reactor.kafka.sender.SenderOptions
 @Configuration
 class KafkaConfig {
     @Bean
+    fun kafkaTelemetry(openTelemetry: OpenTelemetry) = KafkaTelemetry.create(openTelemetry)
+
+    @Bean
     fun <K, V> receiverOptions(
         // Configuration coming from auto configuration prefix "spring.kafka"
-        properties: KafkaProperties
-    ) = ReceiverOptions.create<K, V>(properties.buildConsumerProperties())
+        properties: KafkaProperties,
+        kafkaTelemetry: KafkaTelemetry,
+    ): ReceiverOptions<K, V> {
+        val consumerProperties = properties.buildConsumerProperties()
+        consumerProperties.putAll(kafkaTelemetry.metricConfigProperties())
+        return ReceiverOptions.create(consumerProperties)
+    }
 
     @Bean
     fun <K, V> senderOptions(
         // Configuration coming from auto configuration prefix "spring.kafka"
-        properties: KafkaProperties
-    ) = SenderOptions.create<K, V>(properties.buildProducerProperties())
+        properties: KafkaProperties,
+        kafkaTelemetry: KafkaTelemetry,
+    ): SenderOptions<K, V> {
+        val producerProperties = properties.buildProducerProperties()
+        producerProperties.putAll(kafkaTelemetry.metricConfigProperties())
+        return SenderOptions.create(producerProperties)
+    }
 }
